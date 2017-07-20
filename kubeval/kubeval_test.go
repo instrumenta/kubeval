48b19e46b07bad75f65bd371cf11f07a7b8964bb
Gareth Rushgrove <gareth@morethanseven.net>
2017-07-20 15:23:10 +0100

    Added tests using fixtures to Go unit tests

--- kubeval/kubeval_test.go
+++ kubeval/kubeval_test.go
@@ -1,12 +1,63 @@
 package kubeval
 
-import "testing"
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+)
 
 func TestValidateBlankInput(t *testing.T) {
 	blank := []byte("")
 	_, err := Validate(blank, "sample")
 	if err == nil {
 		t.Errorf("Validate should fail when passed a blank string")
+	}
+}
+
+func TestValidateValidInputs(t *testing.T) {
+	var tests = []string{
+		"valid.yaml",
+		"valid.json",
+		"multi_valid.yaml",
+		"int_or_string.yaml",
+	}
+	for _, test := range tests {
+		filePath, _ := filepath.Abs("../fixtures/" + test)
+		fileContents, _ := ioutil.ReadFile(filePath)
+		_, err := Validate(fileContents, test)
+		if err != nil {
+			t.Errorf("Validate should pass when testing valid configuration in " + test)
+		}
+	}
+}
+
+func TestValidateInvalidInputs(t *testing.T) {
+	var tests = []string{
+		"blank.yaml",
+		"missing-kind.json",
+	}
+	for _, test := range tests {
+		filePath, _ := filepath.Abs("../fixtures/" + test)
+		fileContents, _ := ioutil.ReadFile(filePath)
+		_, err := Validate(fileContents, test)
+		if err == nil {
+			t.Errorf("Validate should not pass when testing invalid configuration in " + test)
+		}
+	}
+}
+
+func TestValidateInputsWithErrors(t *testing.T) {
+	var tests = []string{
+		"invalid.yaml",
+		"multi_invalid.yaml",
+	}
+	for _, test := range tests {
+		filePath, _ := filepath.Abs("../fixtures/" + test)
+		fileContents, _ := ioutil.ReadFile(filePath)
+		results, _ := Validate(fileContents, test)
+		if len(results[0].Errors) == 0 {
+			t.Errorf("Validate should not pass when testing invalid configuration in " + test)
+		}
 	}
 }
 
