478f10e84e25d1e9b699fe7af88688d6329b3907
Ian Howell <ian.howell0@gmail.com>
2019-07-16 16:56:45 -0500

    This adds the ability to force colored output
    Feature suggested in https://github.com/instrumenta/kubeval/issues/87

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 	"runtime"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
@@ -21,6 +22,10 @@ var (
 	commit  = "none"
 	date    = "unknown"
 )
+
+// forceColor tells kubeval to use colored output even if
+// stdout is not a TTY
+var forceColor bool
 
 // RootCmd represents the the command to run when kubeval is run
 var RootCmd = &cobra.Command{
@@ -42,6 +47,10 @@ var RootCmd = &cobra.Command{
 			} else {
 				windowsStdinIssue = true
 			}
+		}
+		// Assert that colors will definitely be used if requested
+		if forceColor {
+			color.NoColor = false
 		}
 		// We detect whether we have anything on stdin to process if we have no arguments
 		// or if the argument is a -
@@ -119,6 +128,7 @@ func init() {
 	RootCmd.Flags().BoolVarP(&kubeval.OpenShift, "openshift", "", false, "Use OpenShift schemas instead of upstream Kubernetes")
 	RootCmd.Flags().BoolVarP(&kubeval.Strict, "strict", "", false, "Disallow additional properties not in schema")
 	RootCmd.Flags().BoolVarP(&kubeval.IgnoreMissingSchemas, "ignore-missing-schemas", "", false, "Skip validation for resource definitions without a schema")
+	RootCmd.Flags().BoolVarP(&forceColor, "force-color", "", false, "Force colored output even if stdout is not a TTY")
 	RootCmd.SetVersionTemplate(`{{.Version}}`)
 	viper.BindPFlag("schema_location", RootCmd.Flags().Lookup("schema-location"))
 	RootCmd.PersistentFlags().StringP("filename", "f", "stdin", "filename to be displayed when testing manifests read from stdin")
